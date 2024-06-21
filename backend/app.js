1a4b57ab3ecbcd515c7a53f489d4db412ea6203d
farsi8273 <mohsalman827332@gmail.com>
2024-06-21 21:39:42 +0530

    new

--- backend/app.js
+++ backend/app.js
@@ -53,10 +53,7 @@ app.use('/', (req, res) => {
 app.use(errorHandler);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   console.log('Connected to MongoDB');
   app.listen(PORT, () => {
